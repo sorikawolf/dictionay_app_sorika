2416c8f0986080aeadae0811049947eea9754cc2
sorikawolf <jennsorikawolf@gmail.com>
2026-08-10 15:59:09 -0400

    Created react app, imported boostrap and hosted on Netlify

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to my dictionary app!</header>
+    </div>
+  );
+}
+
+export default App;
