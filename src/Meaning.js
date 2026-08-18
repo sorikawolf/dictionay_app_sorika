54fc26b67e748c361127a6c30b5697e826269c15
sorikawolf <jennsorikawolf@gmail.com>
2026-08-18 11:27:52 -0400

    fixed code errors

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,17 +7,15 @@ export default function Meaning(props) {
   return (
     <div className="meaning">
       <h4 className="partOfSpeech">{props.meaning.partOfSpeech}</h4>
-      <p className="definition">
-        <strong>Definition: </strong>
-        {props.meaning.definition}
-      </p>
-      <p>
+
+      <strong>Definition: </strong>
+      {props.meaning.definition}
+
+      <div>
         {" "}
         <Example example={props.meaning.example} />
-      </p>
-      <p>
         <Synonyms synonyms={props.meaning.synonyms} />
-      </p>
+      </div>
     </div>
   );
 }
